36f3da509bbf89134ec1b83b7e8a98a3d50dbee4
Дмитро Скороходов <dimon.skorohodov@gmail.com>
2022-09-21 10:08:18 +0300

    #9

--- #source/js/validation.js
+++ #source/js/validation.js
@@ -1,6 +1,11 @@
 const mainForm = document.forms.subscribe;
 const mainFormInput = mainForm.nameInput;
 const mainFormInputPlaceholder = mainFormInput.placeholder;
+const searchForm = document.forms.search;
+const searchFormInput = searchForm.searchInput;
+const searchFormInputPlaceholder = searchFormInput.placeholder;
+const buttonSubscribe = document.querySelector(".email__button");
+const buttonSearch = document.querySelector(".form-body-main__btn");
 
 function emailTest(input) {
   return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
@@ -13,3 +18,17 @@ mainFormInput.addEventListener("blur", function (e) {
   mainFormInput.placeholder = mainFormInputPlaceholder;
 });
 
+mainFormInput.addEventListener("focus", function (e) {
+  searchFormInput.placeholder = "";
+});
+mainFormInput.addEventListener("blur", function (e) {
+  searchFormInput.placeholder = searchFormInputPlaceholder;
+});
+
+buttonSubscribe.addEventListener("click", function (event) {
+  event.preventDefault();
+});
+
+buttonSearch.addEventListener("click", function (event) {
+  event.preventDefault();
+});
